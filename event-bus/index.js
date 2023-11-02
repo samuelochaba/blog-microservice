2c2466aad2796df35b6d1757fab07ff114e821b3
samuelochaba <nerdysam5826@gmail.com>
2023-11-02 08:34:43 +0100

    __

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,28 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { randomBytes } = require("crypto");
-// const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-
 app.use(bodyParser.json());
-// app.use(cors());
-
-const commentsByPostId = {};
-
-// app.get("/posts/:id/comments", (req, res) => {
-//   res.send(commentsByPostId[req.params.id] || []);
-// });
 
 app.post("/events", (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
-
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
   res.send({ status: "OK" });
 });
 
-app.listen(4005, () => console.log("Listening on 4005"));
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+});
